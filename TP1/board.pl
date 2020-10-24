eee0d7728d552a99792cf827ac1f282375ca690d
Pedro Azevedo <pedrag1234@gmail.com>
2020-10-24 18:44:59 +0100

    Progress on the Board

--- TP1/board.pl
+++ TP1/board.pl
@@ -1,17 +1,65 @@
+:- use_module(library(lists)).
+:- use_module(library(random)).
+
+getRep(none,'.').
+getRep(white,'W').
+getRep(black,'B').
+getRep(gray,'G').
+getRep(yellow,'Y').
+getRep(b_yellow,'BY').
+getRep(w_yellow,'WY').
+getRep(joker,'J').
+getRep(_,_):-
+    write('Unknown representation'),
+    nl,
+    fail.
+
+printRep(L):-
+    printColumnSep,
+    write(L),
+    printColumnSep.
 
 /* creates a 10x10 empty board*/
-emptyBoard(
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[0],[1],[],[],[],[]],
-    [[],[],[],[],[1],[0],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]]
-    ).
+createEmptyBoard(Board,N):-
+   createBoard(Board,N,0).
+
+createBoard(_,N,Lines).
+createBoard([FirstRow|OtherRows],N,Lines):-
+    Lines1 is (Lines + 1),
+    createBoardLine(FirstRow,N),
+    createBoard(OtherRows,N,Lines1).
+
+createBoardLine(_,0).
+createBoardLine([Elem1|OtherElem],N):-
+    Elem1 = none,
+    N1 is N - 1,
+    createBoardLine(OtherElem,N1).
+
+
+printBoard(Board, N):-
+    printHeader,
+    printRows(Board, N).
+
+
+printRows(_, 0).
+printRows([FirstElem|OtherElem], N):-
+    printRowNumber(N),
+    N1 is N-1,
+    printRow(FirstElem),
+    nl,
+    printRowSep,
+    printRows(OtherElem,N1).
+
+    
+printRow(_, 0).
+printRow([FirstElem|OtherElem]):-
+    getRep(FirstElem,Label),
+    printRep(Label),
+    printRow(OtherElem).
+
+printRowNumber(N):-
+    write(N),
+    printColumnSep.
 
 printRowSep:-
     write('|----|----|----|----|----|----|----|----|----|----|'),
@@ -24,6 +72,7 @@ printHeader:-
 printColumnSep:-
     write('|').
 
-
-    
+testBoard:-
+    createEmptyBoard(B0,10),
+    printBoard(Board,10).
 
