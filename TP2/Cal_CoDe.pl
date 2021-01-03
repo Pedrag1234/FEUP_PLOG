4acb522e813e897c925b9217efd19406986575cb
Diogo <diogosousa990@gmail.com>
2021-01-03 20:35:04 +0000

    Added starting team list

--- TP2/Cal_CoDe.pl
+++ TP2/Cal_CoDe.pl
@@ -1,30 +1,32 @@
 :- use_module(library(clpfd)).
 
-equipa('FC Porto','Porto').
-equipa('Benfica','Lisboa').
-equipa('Sporting','Lisboa').
-equipa('Boavista','Porto').
-equipa('SC Braga','Braga').
-equipa('B SAD','Lisboa').
-equipa('Gil Vicente','Guimaraes').
-equipa('Famalicao','Vila Nova de Famalicao').
-equipa('Farense','Faro').
-equipa('Maritimo','Funchal').
-equipa('Pacos de Ferreira','Pacos de Ferreira').
-equipa('Nacional','Funchal').
-equipa('Rio Ave','Vila do Conde').
-equipa('Tondela','Tondela').
-equipa('Vitoria de Guimaraes','Guimaraes').
-equipa('Santa Clara','Ponta Delgada').
-equipa('Portimonense','Portimao').
-equipa('Moreirense','Moreira de Conegos').
+%equipa('FC Porto','Porto').
+%equipa('Benfica','Lisboa').
+%equipa('Sporting','Lisboa').
+%equipa('Boavista','Porto').
+%equipa('SC Braga','Braga').
+%equipa('B SAD','Lisboa').
+%equipa('Gil Vicente','Guimaraes').
+%equipa('Famalicao','Vila Nova de Famalicao').
+%equipa('Farense','Faro').
+%equipa('Maritimo','Funchal').
+%equipa('Pacos de Ferreira','Pacos de Ferreira').
+%equipa('Nacional','Funchal').
+%equipa('Rio Ave','Vila do Conde').
+%equipa('Tondela','Tondela').
+%equipa('Vitoria de Guimaraes','Guimaraes').
+%equipa('Santa Clara','Ponta Delgada').
+%equipa('Portimonense','Portimao').
+%equipa('Moreirense','Moreira de Conegos').
 
+%Equipa: Nome-Cidade-JogosCasa-JogosFora
+equipas(['FCPorto-Porto-0-0', 'Benfica-Lisboa-0-0', 'Sporting-Lisboa-0-0', 'Boavista-Porto-0-0', 'SCBraga-Braga-0-0', 'BSAD-Lisboa-0-0',
+       'GilVicente-Guimaraes-0-0', 'Famalicao-VilaNovaDeFamalicao-0-0', 'Farense-Faro-0-0', 'Maritimo-Funchal-0-0',
+       'PacosDeFerreira-PacosDeFerreira-0-0', 'Nacional-Funchal-0-0', 'RioAve-VilaDoConde-0-0', 'Tondela-Tondela-0-0',
+       'VitoriaDeGuimaraes-Guimaraes-0-0', 'SantaClara-PontaDelgada-0-0', 'Portimonense-Portimao-0-0', 'Moreirense-MoreiraDeConegos-0-0']).
 
 defineTeams(Teams):-
     findall([Name-Location], equipa(Name,Location), Teams).
-
-
-    
 
 %generateSeason():-.
 
