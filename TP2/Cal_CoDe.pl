46187184793103fa613cc4f4a603af84f402d5e8
Diogo <diogosousa990@gmail.com>
2021-01-03 21:53:46 +0000

    Added alternate lap generation

--- TP2/Cal_CoDe.pl
+++ TP2/Cal_CoDe.pl
@@ -19,19 +19,36 @@
 %equipa('Portimonense','Portimao').
 %equipa('Moreirense','Moreira de Conegos').
 
-%Equipa: Nome-Cidade-JogosCasa-JogosFora
-equipas(['FCPorto-Porto-0-0', 'Benfica-Lisboa-0-0', 'Sporting-Lisboa-0-0', 'Boavista-Porto-0-0', 'SCBraga-Braga-0-0', 'BSAD-Lisboa-0-0',
-       'GilVicente-Guimaraes-0-0', 'Famalicao-VilaNovaDeFamalicao-0-0', 'Farense-Faro-0-0', 'Maritimo-Funchal-0-0',
-       'PacosDeFerreira-PacosDeFerreira-0-0', 'Nacional-Funchal-0-0', 'RioAve-VilaDoConde-0-0', 'Tondela-Tondela-0-0',
-       'VitoriaDeGuimaraes-Guimaraes-0-0', 'SantaClara-PontaDelgada-0-0', 'Portimonense-Portimao-0-0', 'Moreirense-MoreiraDeConegos-0-0']).
+%Team: Name-City-HomeGames-VisitorGames
+teams(['FCPorto'-'Porto'-0-0, 'Benfica'-'Lisboa'-0-0, 'Sporting'-'Lisboa'-0-0, 'Boavista'-'Porto'-0-0, 'SCBraga'-'Braga'-0-0, 'BSAD'-'Lisboa'-0-0,
+       'GilVicente'-'Guimaraes'-0-0, 'Famalicao'-'VilaNovaDeFamalicao'-0-0, 'Farense'-'Faro'-0-0, 'Maritimo'-'Funchal'-0-0,
+       'PacosDeFerreira'-'PacosDeFerreira'-0-0, 'Nacional'-'Funchal'-0-0, 'RioAve'-'VilaDoConde'-0-0, 'Tondela'-'Tondela'-0-0,
+       'VitoriaDeGuimaraes'-'Guimaraes'-0-0, 'SantaClara'-'PontaDelgada'-0-0, 'Portimonense'-'Portimao'-0-0, 'Moreirense'-'MoreiraDeConegos'-0-0]).
 
-defineTeams(Teams):-
-    findall([Name-Location], equipa(Name,Location), Teams).
+%defineTeams(Teams):-
+%    findall([Name-Location], equipa(Name,Location), Teams).
 
-%generateSeason():-.
+generateLap(Teams, ['FCPorto'-'Benfica'], Teams).
 
-%generateLap():-.
+generateSeason(Teams):-
+    generateLap(Teams, Games, NewTeams),
+    generateAlternateLap(NewTeams, Games, NewTeams2),
+    print(NewTeams2).
 
+addGame([], _, TeamsAcc, TeamsAcc).
+addGame([Team|T], Game, TeamsAcc, NewTeams):-
+    Game = TeamHome-TeamVisitor,
+    Team = Name-City-HomeGames-VisitorGames,
+    ((Name = TeamHome, NewHomeGames is HomeGames + 1, NewTeam = Name-City-NewHomeGames-VisitorGames, addGame(T, Game, [NewTeam|TeamsAcc], NewTeams));
+    (Name = TeamVisitor, NewVisitorGames is VisitorGames + 1, NewTeam = Name-City-HomeGames-NewVisitorGames, addGame(T, Game, [NewTeam|TeamsAcc], NewTeams));
+    (addGame(T, Game, [Team|TeamsAcc], NewTeams))).
+
+generateAlternateLap(NewTeams, [], NewTeams).
+generateAlternateLap(Teams, [Game|T], NewTeams):-
+        Game = TeamHome-TeamVisitor,
+        AlternateGame = TeamVisitor-TeamHome,
+        addGame(Teams, AlternateGame, [], TempTeams),!,
+        generateAlternateLap(TempTeams, T, NewTeams).
 %generateMatches(LapMatches):-.
 
 %generateMatch(LapMatches):-.
